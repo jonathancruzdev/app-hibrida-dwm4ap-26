b045b39a6097b62a89407375a0d7d2046fe8d3c2
Jonathan Cruz <jonathan.cruz@davinci.edu.ar>
2026-03-20 10:03:07 -0300

    Semana 01 - Módulos

--- semana-01/index.js
+++ semana-01/index.js
@@ -1,8 +1,50 @@
 import Persona from "./Persona.js";
+import os from 'os'
+import fs from 'fs/promises'
+import { error } from "console";
+const path = 'notas.txt';
+const data = 'TExto nuevo';
 
-const nombre = 'Sofia';
-console.log(`Hola ${nombre}`);
+fs.readFile( path).then( (data) => {
+    console.log(data.toString());
+}).catch( error => console.error(error))
+
+fs.writeFile(path, data).then( () => {
+    console.log('ARchivo guardado');
+}).catch ( error => console.error(error))
+
+/*
+fs.readFile(path, (error, data) => {
+    if(error){
+        console.error(error);
+    } else {
+        console.log(data.toString());
+    }
+
+    fs.writeFile(path, data, ( error) => {
+        if( error) {
+            console.error(error);
+        }else {
+            console.log('Todo Correcto!');
+        }
+    } )
 
 
-const p1 = new Persona('Carlos', 'Ruiz');
-p1.saludar();
+})
+*/
+
+
+
+
+
+/* console.log( os.platform());
+console.log( os.arch() );
+console.log( os.cpus() ); */
+
+
+// const nombre = 'Sofia';
+//console.log(`Hola ${nombre}`);
+
+
+//const p1 = new Persona('Carlos', 'Ruiz');
+//p1.saludar();
