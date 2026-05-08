61623b7f14f0775ddda09500cd7564303966f7c6
Jonathan Cruz <jonathan.cruz@davinci.edu.ar>
2026-05-08 10:50:25 -0300

    Semana 07 - React: Introducción JSX

---
+++ semana-07/src/data/movies.js
@@ -0,0 +1,32 @@
+export const movies = [
+  {
+    id: 1,
+    title: "Avatar: Fuego y ceniza ",
+    description: "Jake Sully y Neytiri enfrentan una nueva amenaza en Pandora: los Ash People, una tribu Na'vi violenta y sedienta de poder, liderada por la implacable Varang. ",
+    image: "./avatar.webp"
+  },
+  {
+    id: 2,
+    title: "Interstellar",
+    description: "Un grupo de exploradores hacen uso de un agujero de gusano recientemente descubierto para superar las limitaciones de los viajes espaciales tripulados y vencer las inmensas distancias que tiene un viaje interestelar.",
+    image: "./interstellar.webp"
+  },
+  {
+    id: 3,
+    title: "The Matrix",
+    description: "Un hacker descubre la verdad sobre la realidad y su papel en la guerra.",
+    image: "https://via.placeholder.com/300x200?text=Matrix"
+  },
+  {
+    id: 4,
+    title: "The Dark Knight",
+    description: "Batman enfrenta al Joker en una batalla por el alma de Gotham.",
+    image: "https://via.placeholder.com/300x200?text=Batman"
+  },
+  {
+    id: 5,
+    title: "Avengers: Endgame",
+    description: "Los héroes restantes intentan revertir el chasquido de Thanos.",
+    image: "https://via.placeholder.com/300x200?text=Avengers"
+  }
+];
