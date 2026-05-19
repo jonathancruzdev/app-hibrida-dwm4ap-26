e00ef354fa211f476bd294266213f2b87744d609
Jonathan Cruz <jonathan.cruz@davinci.edu.ar>
2026-05-19 09:42:52 -0300

    Semana 10 - Componentes

---
+++ semana-10/src/App.jsx
@@ -0,0 +1,21 @@
+import reactLogo from './assets/react.svg'
+
+import Home from './pages/Home';
+
+import './App.css'
+
+
+function App() {
+  const semana = 'Semana 10';
+  const span = <span> Algo </span>;
+
+  return (
+    <>
+      <Home />
+    </>
+
+  )
+}
+
+export default App
+
---
+++ semana-10/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer className='footer'>
+      <p>DV | Movie APP | 2026</p>
+    </footer>
+  )
+}
