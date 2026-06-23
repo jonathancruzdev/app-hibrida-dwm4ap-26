5d951e2b603345a567df2a1cf73223b38ca87348
Jonathan Cruz <jonathan.cruz@davinci.edu.ar>
2026-06-23 09:28:19 -0300

    Corrección de variables de entorno

--- semana-13/backend/src/controllers/AuthController.js
+++ semana-13/backend/src/controllers/AuthController.js
@@ -84,6 +84,7 @@ export const login = async (req, res) => {
         }
     });
   } catch (error) {
+        console.error(error)
         res.status(500).json({
             message: "Error al iniciar sesión"
         });
--- semana-13/backend/src/routes/index.js
+++ semana-13/backend/src/routes/index.js
@@ -1,8 +1,9 @@
 import PointRouter from "./PointRoute.js";
 import AuthRouter from "./AuthRoute.js";
 
-const routerAPI = (app) =>{
-    app.use('/api/points', PointRouter)
+const routerAPI = (app) => {
+    app.use('/api/points', PointRouter);
+    app.use('/api/auth', AuthRouter);
     
 }
 export default routerAPI;
